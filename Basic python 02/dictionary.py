aeba1481cfe496f4f04f830f7c6c97b3bd3aef58
imranme <tusharimran914076@gmail.com>
2023-09-27 21:28:25 +0600

    today code of tuples and set

---
+++ Basic python 02/dictionary.py
@@ -0,0 +1,16 @@
+num = [12, 34, 55, 54, 66, 77.12]
+person1 = ['kala chan', 'kalipur', 23, 'student']
+
+#key value pair
+#dictionary
+#object
+# hash table
+#overlap with set
+#{key: value, key:value, key: value}
+
+person = {'name': 'kala chan', 'addres': 'dhaka', 'age': 23, 'job': 'facbooker'}
+
+print(person)
+print(person['job'])
+print(person.keys())
+print(person.values())
