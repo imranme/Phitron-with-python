3b5cd61dc9c8048ebe36a94bb4689f51e8384c87
imranme <tusharimran914076@gmail.com>
2023-09-27 23:57:54 +0600

    solve codefors problem

---
+++ practice_problem_two.py/first.py
@@ -0,0 +1,50 @@
+# T = int(input())
+
+# for _ in range(T):
+#     N = int(input()) 
+#     print(N)
+
+# lst = []
+
+# n= int(input())
+
+# for i in range(0, n):
+
+# Input
+n = int(input())
+arr = input().split()
+
+reverse_arr = arr[::-1]
+
+if arr == reverse_arr:
+    print("YES")
+else:
+    print("NO")
+
+
+n = int(input())
+arr = input().split()
+
+reverse_arr = arr[::-1]
+
+if arr == reverse_arr:
+    print("YES")
+else:
+    print("NO")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practice_problem_two.py/sum_digits.py
@@ -0,0 +1,8 @@
+n = int(input())
+digits = input()
+
+sum = 0
+for digit in digits:
+    digit_int = int(digit)
+    sum += digit_int
+print(sum)
